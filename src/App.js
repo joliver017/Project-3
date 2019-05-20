98e3ac4da96345fdc384b4b9949674946bf1f3a1
Oliver 2wist <joliver017@gmail.com>
2019-05-19 23:08:39 -0500

    created react app, uikit working for styling, basic page with name and intro

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-align-center">
+      <h1 className="uk-card-title uk-text-center">SWYSH</h1>
+      <h4 className="uk-text-center">share what you see, hear</h4>
+      <p>Welcome to SWYSH, an app whose sole purpose is for users to share pictures
+        of what they think or see when they hear a song.</p>
+
+      <p>What do you see in your head when you listen to a song? Do you see a 
+        sunset at a lake? The neon lights of a traffic light at night?</p>
+
+      <p className="uk-text-center"><strong>Share what you see, here.</strong></p>
+    </div>
+  );
+}
+
+export default App;
