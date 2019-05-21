35adb523441b8b6f9080237a8169ee070dddbbeb
Oliver 2wist <joliver017@gmail.com>
2019-05-20 22:13:04 -0500

    added code for axios request and server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+const PORT = 8080
+
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+//routing
+app.post('/', (req, res, next)=> {
+	console.log('server post username: ');
+	console.log(req.body.username)
+	res.end()
+})
+
+// Starting Server 
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
