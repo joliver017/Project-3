bee35f8f9619d22cbad702059e4aceecf4a43e12
Oliver 2wist <joliver017@gmail.com>
2019-05-22 00:31:31 -0500

    added redirect to login after signup

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 class SignupForm extends Component {
@@ -40,42 +41,45 @@ class SignupForm extends Component {
 			})
 	}
 	render() {
+		if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } else {
+			return (
+				<div className="SignupForm">
+					<div className="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-align-center uk-box-shadow-large">
+						{/* <h1 className="uk-card-title uk-text-center">SWYSH</h1> */}
+						<h4 className="uk-text-center">share what you see, hear</h4>
+						<p>Welcome to SWYSH, an app whose sole purpose is for users to share pictures
+						of what they think or see when they hear a song.</p>
 
-		return (
-			<div className="SignupForm">
-				<div className="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-align-center uk-box-shadow-large">
-        			{/* <h1 className="uk-card-title uk-text-center">SWYSH</h1> */}
-					<h4 className="uk-text-center">share what you see, hear</h4>
-					<p>Welcome to SWYSH, an app whose sole purpose is for users to share pictures
-					of what they think or see when they hear a song.</p>
+						<p>What do you see in your head when you listen to a song? Do you see a 
+						sunset at a lake? The neon lights of a traffic light at night?</p>
+						<p className="uk-text-center"><strong>Share what you see, here.</strong></p>
+					</div>
 
-					<p>What do you see in your head when you listen to a song? Do you see a 
-					sunset at a lake? The neon lights of a traffic light at night?</p>
-					<p className="uk-text-center"><strong>Share what you see, here.</strong></p>
+					<h1 className="uk-text-center">Sign-up</h1>
+					<label htmlFor="username">Username: </label>
+					<input
+						className="uk-input uk-margin uk-width-1-2"
+						type="text"
+						name="username"
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
+					<br></br>
+					<label htmlFor="password">Password: </label>
+					<input
+						className="uk-input uk-margin uk-width-1-2"
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					
+					<button className="uk-button uk-button-default uk-align-center" onClick={this.handleSubmit}>Sign up</button>
 				</div>
-
-				<h1 className="uk-text-center">Sign-up</h1>
-				<label htmlFor="username">Username: </label>
-				<input
-                    className="uk-input uk-margin uk-width-1-2"
-					type="text"
-					name="username"
-					value={this.state.username}
-					onChange={this.handleChange}
-				/>
-                <br></br>
-				<label htmlFor="password">Password: </label>
-				<input
-                    className="uk-input uk-margin uk-width-1-2"
-					type="password"
-					name="password"
-					value={this.state.password}
-					onChange={this.handleChange}
-				/>
-				
-				<button className="uk-button uk-button-default uk-align-center" onClick={this.handleSubmit}>Sign up</button>
-			</div>
-		)
+			)
+		}
 	}
 }
 
