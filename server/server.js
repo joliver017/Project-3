1da28177768e28552f823be28180960630e3999e
Oliver 2wist <joliver017@gmail.com>
2019-05-30 19:23:15 -0500

    trying to deploy again, added new code found on google

--- server/server.js
+++ server/server.js
@@ -23,6 +23,11 @@ app.use(bodyParser.json())
 // Serve up static assets (usually on Heroku)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
+	// Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Sessions
